7ca2887a36289f060016f3f2561f978f62e63025
yuvalgarti <yuvalgarti@gmail.com>
2018-12-15 17:06:36 +0200

    Project Creation
    Creating the backbone for the Django project

---
+++ Collaborative_Music_Arts/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Collaborative_Music_Arts project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Collaborative_Music_Arts.settings')
+
+application = get_wsgi_application()
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Yuval\Collaborative_Music_Arts\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
