c3b722677d441a4b0d121f4dd6c8fa79e8f75701
yuvalgarti <yuvalgarti@gmail.com>
2019-05-26 13:13:19 +0300

    Added tests for models creation

--- compositions/tests/CreateModelsTest.py
+++ compositions/tests/CreateModelsTest.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.models import User
+from django.db.models import QuerySet
 from django.test import Client, TestCase
 from django.urls import reverse
+
+from compositions.models import Composition, Track
 from .ReverseWithParams import my_reverse
 
 
@@ -19,3 +22,25 @@ class CreateModelsTest(TestCase):
         self.assertRedirects(response, show_composition_path)
         response = self.client.get(show_composition_path)
         self.assertEqual(response.status_code, 200)
+
+    def test_create_composition(self):
+        response = self.client.post(reverse('create_composition'), {'name': 'compo1'})
+        show_composition_path = reverse('show_composition', kwargs={'composition_id': 1})
+        self.assertRedirects(response, show_composition_path)
+        response = self.client.get(show_composition_path)
+        self.assertEqual(response.status_code, 200)
+
+"""
+    # Migration error
+    def test_create_variation(self):
+        compo = Composition()
+        compo.save(user=self.user)
+        track = Track()
+        track.save(user=self.user, composition_id=compo.id)
+        show_variation_path = reverse('show_variation', kwargs={'variation_id': 1})
+        response = self.client.post(reverse('create_variation', kwargs={'composition_id': compo.id}),
+                                    {'name': 'vari1', 'tracks': [str(track.id)]})
+        self.assertRedirects(response, show_variation_path)
+        response = self.client.get(show_variation_path)
+        self.assertEqual(response.status_code, 200)
+"""
