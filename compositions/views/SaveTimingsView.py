2d8c875f7c8aa63de94dd223d9b1724f2a649892
yuvalgarti <yuvalgarti@gmail.com>
2019-06-19 22:12:59 +0300

    Fixed save timing problem

--- compositions/views/SaveTimingsView.py
+++ compositions/views/SaveTimingsView.py
@@ -11,8 +11,11 @@ class SaveTimingsView(View):
         variation_id = kwargs['variation_id']
         if Variation.objects.get(id=variation_id).creator.id == request.user.id:
             for t in js_tracks:
-                py_track = Track.objects.get(track_file=js_tracks[t][1][len('/media//'):]).id
-                track_in_vari = TrackInVariation.objects.get(track_id=py_track, variation_id=variation_id)
-                track_in_vari.start_timing = js_tracks[t][0]
-                track_in_vari.save()
+                try:
+                    py_track = Track.objects.get(track_file=js_tracks[t][1][len('/media//'):]).id
+                    track_in_vari = TrackInVariation.objects.get(track_id=py_track, variation_id=variation_id)
+                    track_in_vari.start_timing = js_tracks[t][0]
+                    track_in_vari.save()
+                except Track.DoesNotExist or Track.MultipleObjectsReturned:
+                    pass
         return redirect('show_variation', variation_id)
