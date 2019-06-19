9cc34662bc197a6ed452a8fbfa342444d334b7b5
yuvalgarti <yuvalgarti@gmail.com>
2019-06-19 21:44:25 +0300

    Fixed alignment of variation form

--- compositions/forms/VariationForm.py
+++ compositions/forms/VariationForm.py
@@ -8,7 +8,9 @@ class VariationForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         self.composition_id = kwargs.pop('composition_id')
         super(forms.ModelForm, self).__init__(*args, **kwargs)
-        self.fields['tracks'] = forms.ModelMultipleChoiceField(Track.objects.filter(composition_id=self.composition_id),widget=forms.CheckboxSelectMultiple)
+        self.fields['tracks'] = forms.ModelMultipleChoiceField(Track.objects.filter(composition_id=self.composition_id),
+                                                               widget=forms.CheckboxSelectMultiple(
+                                                                   attrs={'class': 'text-left'}))
 
     class Meta:
         model = Variation
